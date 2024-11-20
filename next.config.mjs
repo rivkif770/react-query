53718005460ae4769ef7d7cc275ec3a22c2ba8f0
rivkif770 <frieman@g.jct.ac.il>
2024-11-20 15:49:42 +0200

    react-query

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  env: {
+    PUBLIC_DB_CONNECTION: "mongodb+srv://rivkif7707:2xh7QvXDp8ASL6Pc@cluster0.eyuho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+},};
 
 export default nextConfig;
+
+
+
+
+
+
+
